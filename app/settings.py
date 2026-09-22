45fdd9467a01eb3bf5f0b2a501bd1c0600b77fb2
gallegovwilliam-ai <gallegovwilliam@gmail.com>
2026-09-22 16:33:25 -0500

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -212,7 +212,7 @@ STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static_files
 STORAGES = {
     'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
     'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage'}
-#STATIC_ROOT = '/opt/softandplay/static_files/'
+#STATIC_ROOT = '/opt/softandplay/static_files/'}
 
 #MEDIA_ROOT = '/opt/softandplay/media_files'
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media_files'))
