8796e0a5df080246f15c988e169c384763fb10c1
gallegovwilliam-ai <gallegovwilliam@gmail.com>
2026-09-22 17:56:17 -0500

    Update models.py

--- partidas/models.py
+++ partidas/models.py
@@ -154,4 +154,29 @@ class Partida75(models.Model):
     def __str__(self):
         return  "Partida Bingo 75 #" + str(self.partida) + " / " + str(self.fecha) + " / " + self.descripcion
     monto_carton = models.DecimalField(max_digits=14, decimal_places=2, blank=True, default=0)
- {
+ {class Meta:
+        ordering = ['id']
+        constraints = [
+            models.UniqueConstraint(fields=['fecha', 'partida'], name='uniq_partida75_fecha_num')
+        ] + [
+            models.CheckConstraint(check=models.Q(**{f'{n}__gte': 0}), name=f'partida75_{n}_nonnegative') 
+            for n in ['monto_1','monto_2','monto_3','monto_4','monto_5','monto_6','monto_7','monto_8','monto_9','monto_10','monto_carton']
+        ] + [
+            models.CheckConstraint(check=models.Q(**{f'{n}__gte': 0}) & models.Q(**{f'{n}__lte': 100}), name=f'partida75_{n}_range') 
+            for n in ['porciento_1','porciento_2','porciento_3','porciento_4','porciento_5','porciento_6','porciento_7','porciento_8','porciento_9','porciento_10']
+        ]
+
+
+class Jugada75(models.Model):
+    partida = models.ForeignKey(Partida75, on_delete=models.CASCADE)
+    balota = models.CharField(max_length=2, blank=True)
+    orden = models.CharField(max_length=2, blank=True)
+    created_at = models.DateTimeField(editable=False, blank=False, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+        return super(Jugada75, self).save(*args, **kwargs)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['partida', 'balota'], name='uniq_jugada75_partida_balota')]
